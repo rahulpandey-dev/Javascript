eed314a33691c7f8ae8ec9d3247c7f9666e02641
Rahul Pandey <153789767+rahulpandey-dev@users.noreply.github.com>
2024-10-18 19:42:16 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1233445;
+let accountEmail = "Rahul1234@gmail.com"
+var accountPassword = "12345"
+/*
+Prefer not to use scope
+because of block scope or functional scope
+
+*/
+
+let accountState;
+
+
+accountCity = "Jaipur"
+
+
+//accountID = 2344; // Not allowed
+
+accountEmail = "rahul4321@gmail.com";
+accountPassword = "54321";
+accountCity = "Delhi";
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
