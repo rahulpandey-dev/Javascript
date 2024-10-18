744bf1c021b9c83b863f7a6ea7faa5d5caf8420c
Rahul Pandey <153789767+rahulpandey-dev@users.noreply.github.com>
2024-10-18 20:03:27 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as a new version
+
+
+//alert("Rahul"); // we are using nodejs, not the browser
+
+console.log(3
+    +3) // code readability should be high
+console.log(4+4)
+
+// Data Types
+
+
+// Number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+
+let name = "Rahul";
+let age = 22;
+let isStudent = true;
+let state = null;
+
+
+console.log(typeof name );
+console.log(typeof null);
+console.log(typeof undefined);
+
+
