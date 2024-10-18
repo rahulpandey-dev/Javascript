8510355ab3156c3c4ddbc34a9f89ff142c294a7d
Rahul Pandey <153789767+rahulpandey-dev@users.noreply.github.com>
2024-10-18 20:36:03 +0000

    ConversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "92";
+console.log(typeof score);
+
+const valueInNUmber = Number(score);
+console.log(valueInNUmber);
+
+let score1 = "33abs";
+console.log(typeof score);
+
+const valueInNUmber1 = Number(score1);
+console.log(valueInNUmber1);
+
+let isLoggedin = 1;
+
+let checkinlogin = Boolean(isLoggedin);
+console.log(checkinlogin);
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+
+
+
