e3f875acdcf5ed6994e103d63e5495dfdd30e711
Rahul Pandey <153789767+rahulpandey-dev@users.noreply.github.com>
2024-10-19 06:53:42 +0000

    Operayions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,5 +21,35 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 
 
+// Operations
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+let str1 = "Hello";
+let str2 = "Rahul";
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+let gameCounter= 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
 
 
